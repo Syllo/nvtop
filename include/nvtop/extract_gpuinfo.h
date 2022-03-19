ec6eced4e835d38442fe134b12cf184f559431f4
YiFei Zhu <zhuyifei1999@gmail.com>
2022-03-19 05:11:05 -0700

    Make GPU vendors use a hooking mechanism
    So we don't need to hardcode supported vendors in generic
    extract_gpuinfo code.

--- include/nvtop/extract_gpuinfo.h
+++ include/nvtop/extract_gpuinfo.h
@@ -23,11 +23,10 @@
 #define EXTRACT_GPUINFO_H_
 
 #include "nvtop/extract_gpuinfo_common.h"
-#include "nvtop/extract_gpuinfo_nvidia.h"
 
 #include <stdbool.h>
 
-bool gpuinfo_init_info_extraction(uint64_t mask_nvidia, unsigned *devices_count, struct list_head *devices);
+bool gpuinfo_init_info_extraction(uint64_t mask, unsigned *devices_count, struct list_head *devices);
 
 bool gpuinfo_shutdown_info_extraction(struct list_head *devices);
 
